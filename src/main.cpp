e0038034f2c35ec087a4b4e97a433d022e4c38c7
shreyasbg <shreyasbgangurde@gmail.com>
2026-03-06 16:19:16 +0530

    testing webhook trigger

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
 #include <iostream>
 
-int main() { std::cout << "Scientific Calculator (Just testing trigger)\n"; }
+int main() {
+  std::cout << "Scientific Calculator (Just testing trigger AGAIN)\n";
+}
